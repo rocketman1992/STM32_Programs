866881324e52d65468c09b521d405004510cb5cc
Erick <stanley6302@gmail.com>
2023-12-21 14:25:02 -0800

    added HAL files and definitions

--- 01_buzzer/Code/buzz.h
+++ 01_buzzer/Code/buzz.h
@@ -1,13 +1,14 @@
-/*
- * buzz.h
- *
- *  Created on: Dec 21, 2023
- *      Author: Erick
- */
 
 #ifndef BUZZ_H_
 #define BUZZ_H_
 
+/* #define section */
+#define LED_TOGGLE()  HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin)
+
+/* function declarations */
+void Bsp_Init(void);
+void Bsp_Loop(void);
+void Bsp_Led_Show_State_Handle(void);
 
 
 #endif /* BUZZ_H_ */
--- 01_buzzer/Core/Src/main.c
+++ 01_buzzer/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -48,7 +49,6 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -138,42 +138,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, LED_Pin|BEEP_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pins : LED_Pin BEEP_Pin */
-  GPIO_InitStruct.Pin = LED_Pin|BEEP_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : KEY1_Pin */
-  GPIO_InitStruct.Pin = KEY1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(KEY1_GPIO_Port, &GPIO_InitStruct);
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
