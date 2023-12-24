f244ee7079f02c35b2bb693d4d99c7f31fa0569d
Erick <stanley6302@gmail.com>
2023-12-23 16:06:54 -0800

    updated code structure

--- 01_buzzer/Code/buzz.c
+++ 01_buzzer/Code/buzz.c
@@ -7,5 +7,39 @@
 
 void Bsp_Loop(void)
 {
-	Beep(1);
+	if (Key1_is_Pressed() == BEEP_STATE_ON)
+	{
+		Beep(BEEP_STATE_ON);
+	}
+
+}
+
+
+void Beep(uint8_t state)
+{
+	if (state == BEEP_STATE_ON)
+	{
+		BEEP_ON();
+		HAL_Delay(1000);
+		BEEP_OFF();
+
+	}
+	else if (state == BEEP_STATE_OFF)
+	{
+		BEEP_OFF();
+	}
+
+}
+
+
+uint8_t Key1_is_Pressed(void)
+{
+	if (!KEY1_STATE())
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
--- 01_buzzer/Code/buzz.h
+++ 01_buzzer/Code/buzz.h
@@ -6,34 +6,17 @@
 #include "stdint.h"
 #include "main.h"
 
-/* Global variables */
-uint8_t first_time = 0;
 
 /* #define section */
 #define BEEP_STATE_OFF          0
 #define BEEP_STATE_ON           1
 #define BEEP_ON()               HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, SET)
 #define BEEP_OFF()              HAL_GPIO_WritePin(BEEP_GPIO_Port, BEEP_Pin, RESET)
+#define KEY1_STATE()            HAL_GPIO_ReadPin(KEY1_GPIO_Port, KEY1_Pin)
 
 /* function declarations */
 void Bsp_Loop(void);
 void Beep(uint8_t state);
-
-void Beep(uint8_t state)
-{
-	if (state == BEEP_STATE_ON)
-	{
-		BEEP_ON();
-	}
-	else if (state == BEEP_STATE_OFF)
-	{
-		BEEP_OFF();
-	}
-	else
-	{
-
-	}
-
-}
+uint8_t Key1_is_Pressed(void);
 
 #endif /* BUZZ_H_ */
